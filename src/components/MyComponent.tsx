fb57ee918070eb2693fc77ce24a2d6b6b964ea8d
AntonSkyrda <mr.skyrda@gmail.com>
2025-08-22 23:40:45 +0300

    install tailwind lib

--- src/components/MyComponent.tsx
+++ src/components/MyComponent.tsx
@@ -3,7 +3,7 @@ import type {FC} from "react";
 type MyComponentPropType = { text: string }
 
 const MyComponent: FC<MyComponentPropType> = ({text})=> {
-    return <div>{text}</div>
+    return <div className="h-screen flex items-center justify-center bg-slate-900 text-white">{text}</div>
 }
 
 export default MyComponent;
