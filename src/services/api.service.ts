84590edada92cc12593ee9bd3e0a427df287a3c5
AntonSkyrda <mr.skyrda@gmail.com>
2025-08-26 04:26:13 +0300

    done lesson9

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,11 +1,13 @@
 import type {IUser} from "../models/IUser.ts";
 
+const baseUrl = import.meta.env.VITE_API_URL;
+
 export const getUsers = async (): Promise<IUser[]> => {
-    return await fetch("https://jsonplaceholder.typicode.com/users")
+    return await fetch(baseUrl)
         .then(res => res.json());
 }
 
-export const getUser = async (userId: string): Promise<IUser[]> => {
-    return await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+export const getUser = async (userId: string): Promise<IUser> => {
+    return await fetch(baseUrl + userId)
         .then(res => res.json());
 }
