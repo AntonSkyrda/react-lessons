62a9a5ff7fcc6e2d1802ef69124b2ef936a3e7f3
AntonSkyrda <mr.skyrda@gmail.com>
2025-09-09 01:26:49 +0300

    done simple controled form lesson

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
+import {FormComponent} from "./components/form-component/FormComponent.tsx";
 
 function App() {
 
-  return (
-    <>
-    </>
-  )
+    return (
+        <div>
+            <FormComponent/>
+        </div>
+    )
 }
 
 export default App
