3caa884d6c93297dbdc4f72787c8dc4ab0abc162
AntonSkyrda <mr.skyrda@gmail.com>
2025-10-01 22:50:43 +0300

    done auth lesson

--- src/pages/AuthResourcePage.tsx
+++ src/pages/AuthResourcePage.tsx
@@ -1,4 +1,17 @@
+import {useEffect} from "react";
+import {loadAuthProducts, refresh} from "../services/api.services.ts";
+
 export const AuthResourcePage = () => {
+
+    useEffect(() => {
+        loadAuthProducts().then(allProducts => {
+            console.log(allProducts);
+        }).catch(reason => {
+            console.log(reason);
+            refresh().then(() => loadAuthProducts())
+        })
+    }, [])
+
     return (
         <div>
             Auth Resource Page
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,4 +1,14 @@
+import {useEffect} from "react";
+import {login} from "../services/api.services.ts";
+
 export const LoginPage = () => {
+    useEffect(() => {
+        login({
+            username: "emilys",
+            password: "emilyspass",
+            expiresInMin: 1,
+        })
+    })
     return (
         <div>
             Login Page
